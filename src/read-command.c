cf0a8553f63bb6de344618062b135dd856898200
Gautam Gupta <blerb795@gmail.com>
2015-01-08 03:46:32 -0800

    progress!

--- src/read-command.c
+++ src/read-command.c
@@ -24,11 +24,21 @@
 #else
 #include <error.h>
 #endif
+#include "alloc.h"
+#include <stdlib.h>
+#include <stdio.h>
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
+struct command_stream
+{
+  command_t *commands;
+  int command_idx;
+  int num_commands;
+  int maxsize;
+};
 
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
@@ -37,8 +47,26 @@ make_command_stream (int (*get_next_byte) (void *),
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
-  error (1, 0, "command reading not yet implemented");
-  return 0;
+  command_stream_t stream = (command_stream_t) checked_malloc(sizeof(struct command_stream));
+  stream->commands = NULL;
+  stream->command_idx = stream->num_commands = stream->maxsize = 0;
+  stream->commands = (command_t*) checked_malloc(128 * sizeof(command_t));
+  stream->maxsize = 128;
+  int byte;
+  while ((byte = get_next_byte(get_next_byte_argument)) != EOF)
+  {
+    // load the commands
+  }
+//  error (1, 0, "command reading not yet implemented");
+  return stream;
+}
+
+void
+free_command_stream(command_stream_t stream)
+{
+  if (stream->commands)
+    free(stream->commands);
+  free(stream);
 }
 
 command_t
