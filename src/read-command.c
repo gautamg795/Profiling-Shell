cb23aeab44f907b074e0a30f2efb86a6cacc87b5
Gautam Gupta <blerb795@gmail.com>
2015-01-09 04:33:55 -0800

    indent braces as per GNU style

--- src/read-command.c
+++ src/read-command.c
@@ -32,10 +32,10 @@
 #include <ctype.h>
 
 /* FIXME: You may need to add #include directives, macro definitions,
-   static function definitions, etc.  */
+ static function definitions, etc.  */
 
 /* FIXME: Define the type 'struct command_stream' here.  This should
-   complete the incomplete type declaration in command.h.  */
+ complete the incomplete type declaration in command.h.  */
 struct command_stream
 {
   command_t *commands;
@@ -47,31 +47,32 @@ struct command_stream
 
 static int line_num = 0;
 
-char *get_one_line(int (*getbyte) (void *), void *arg)
+char *
+get_one_line(int (*getbyte) (void *), void *arg)
 {
   char* str = checked_malloc(32 * sizeof(char));
   int curLen = 0;
   int maxLen = 32;
   int byte;
   while(true)
-  {
-    if (curLen == maxLen)
     {
+    if (curLen == maxLen)
+      {
       str = (char*)checked_realloc(str, maxLen * 2 * sizeof(char));
       maxLen *= 2;
-    }
+      }
     byte = getbyte(arg);
     if (byte == EOF || byte == '\n')
-    {
+      {
       if (byte == EOF && curLen == 0)
         return NULL; // Only return NULL if we hit EOF without having read anything
       str[curLen] = 0; // Put a null byte in so strlen works correctly
       break;
-    }
+      }
     if (curLen == 0 && isspace(byte)) // discard leading whitespace
       continue;
     str[curLen++] = byte;
-  }
+    }
   line_num++; // For syntax error reporting
   return str;
 }
@@ -90,30 +91,30 @@ make_command_stream (int (*get_next_byte) (void *),
                      void *get_next_byte_argument)
 {
   /* FIXME: Replace this with your implementation.  You may need to
-     add auxiliary functions and otherwise modify the source code.
-     You can also use external functions defined in the GNU C Library.  */
+   add auxiliary functions and otherwise modify the source code.
+   You can also use external functions defined in the GNU C Library.  */
   command_stream_t stream = (command_stream_t) checked_malloc(sizeof(struct command_stream));
   stream->commands = NULL;
   stream->command_idx = stream->num_commands = stream->maxsize = 0;
   stream->commands = (command_t*) checked_malloc(128 * sizeof(command_t));
   stream->maxsize = 128;
   while(1)
-  {
-    if (stream->num_commands == stream->maxsize)
     {
+    if (stream->num_commands == stream->maxsize)
+      {
       stream->commands = (command_t*) checked_realloc(stream->commands,
                                                       (stream->maxsize + 128) * sizeof(command_t));
       stream->maxsize += 128;
-    }
+      }
     command_t cmd = build_command(get_next_byte, get_next_byte_argument, UNPARSED,
                                   NULL);
     if (cmd == NULL) /* Done reading commands */
-    {
+      {
       break; //??
-    }
-      // TODO: Add the command to the stream after we get it
+      }
+    // TODO: Add the command to the stream after we get it
     stream->commands[stream->num_commands++] = cmd;
-  }
+    }
   return stream;
 }
 
@@ -123,7 +124,7 @@ free_command_stream(command_stream_t stream)
   if (stream->commands)
     free(stream->commands);
   free(stream);
-    // !!!: This isn't good enough.
+  // !!!: This isn't good enough.
 }
 
 command_t
@@ -133,56 +134,56 @@ build_command(int (*getbyte) (void *), void *arg, command_tokenization_state sta
   command_t cmd = NULL;
   char *word = NULL;
   if (!line)
-  {
-    while(true)
     {
+    while(true)
+      {
       line = get_one_line(getbyte, arg);
       if (!line)
         return NULL;
       if (strlen(line) > 0)
         break;
+      }
     }
-  }
   cmd = (command_t)checked_malloc(sizeof(struct command));
   size_t original_linelength = strlen(line); // because strtok mangles the line
   
   if (state == UNPARSED)
-  {
+    {
     word = strtok(line, " ");
     if (!word) // TODO: This shouldn't happen.
       error(1, 0, "attempted parsing empty line in build_command()");
     if (strcmp(word, "if") == 0)
-    {
+      {
       cmd->type = IF_COMMAND;
       if (original_linelength > 2 && words_left_on_line(line+3)) // 3 because if\0
-      {
+        {
         char *newline = (char*)checked_malloc((1 + strlen(line + 3)) * sizeof(char));
         strcpy(newline, line+3);
         free(line);
         cmd->u.command[0] = build_command(getbyte, arg, THEN, newline);
-      }
+        }
       else
-      {
+        {
         // No more words on the line, get a new line
         cmd->u.command[0] = build_command(getbyte, arg, THEN, NULL);
-      }
+        }
       
-    }
+      }
     else if (strcmp(word, "while") == 0)
-    {
+      {
       cmd->type = WHILE_COMMAND;
-    }
+      }
     else if (strcmp(word, "until") == 0)
-    {
+      {
       cmd->type = UNTIL_COMMAND;
-    }
+      }
     else
-    {
+      {
       cmd->type = SIMPLE_COMMAND;
       cmd->u.word = &line;
       return cmd;
+      }
     }
-  }
   
   else if (state == THEN) {
     // ..what about if you have if if 1 && if 2 then echo hello fi.. bad syntax
